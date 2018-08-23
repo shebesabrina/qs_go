d9b93d2f073ee8b8ee7fa1101d705c3e710c3873
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-23 15:09:31 -0600

    Implements models for HTTP verbs

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 import(
-  "fmt"
-  "encoding/json"
+  // "fmt"
+  // "encoding/json"
   "log"
   "net/http"
-  "math/rand"
-  "strconv"
+  // "math/rand"
+  // "strconv"
   "github.com/gorilla/mux"
 )
+//Food Struct (Model)
+type Food struct {
+  ID string `json:"id"`
+  Isbn string `json:"isbn"`
+  Name string `json:"name"`
+  Calories string `json:"calories"`
+}
+// Meal Struct (Model)
+type Meal struct {
+  ID string `json:"id"`
+  Isbn string `json:"isbn"`
+  Name string `json:"name"`
+}
+
+//GET all foods
+func getFoods(w http.ResponseWriter, r *http.Request)  {
+
+}
+//GET single food
+func getFood(w http.ResponseWriter, r *http.Request)  {
+
+}
+//GET create food item
+func createFood(w http.ResponseWriter, r *http.Request)  {
+
+}
+//GET update food item
+func updateFood(w http.ResponseWriter, r *http.Request)  {
+
+}
+//GET delete food item
+func deleteFood(w http.ResponseWriter, r *http.Request)  {
+
+}
+
 func main()  {
   //Init Router
-  router := Mux.NewRouter()
+  router := mux.NewRouter()
 
   //Route handlers / endpoints
   router.HandleFunc("/api/v1/foods", getFoods).Methods("GET")
@@ -18,4 +53,6 @@ func main()  {
   router.HandleFunc("/api/v1/foods", createFood).Methods("POST")
   router.HandleFunc("/api/v1/foods/{id}", updateFood).Methods("PUT")
   router.HandleFunc("/api/v1/foods/{id}", deleteFood).Methods("DELETE")
+
+  log.Fatal(http.ListenAndServe(":8000", router))
 }
