b258cfdaca5e6493aff8a455904a1f5cd293d95f
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-23 15:55:21 -0600

    Implements create, update, and delete endpoints for foods

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 import(
   // "fmt"
-  // "encoding/json"
+  "encoding/json"
   "log"
   "net/http"
-  // "math/rand"
-  // "strconv"
+  "math/rand"
+  "strconv"
   "github.com/gorilla/mux"
 )
 //Food Struct (Model)
@@ -44,15 +44,41 @@ func getFood(w http.ResponseWriter, r *http.Request)  {
 }
 //GET create food item
 func createFood(w http.ResponseWriter, r *http.Request)  {
-
+  w.Header().Set("Content-Type", "application/json")
+  var food Food
+  _ = json.NewDecoder(r.Body).Decode(&food)
+  food.ID = strconv.Itoa(rand.Intn(10000)) //Mock ID - not safe for production
+  foods = append(foods, food)
+  json.NewEncoder(w).Encode(food)
 }
 //GET update food item
 func updateFood(w http.ResponseWriter, r *http.Request)  {
-
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range foods {
+    if item.id == params["id"] {
+      foods = append(foods[:index], foods[index+1:]...)
+      var food Food
+      _ = json.NewDecoder(r.Body).Decode(&food)
+      food.ID = strconv.Itoa(rand.Intn(10000)) //Mock ID - not safe for production
+      foods = append(foods, food)
+      json.NewEncoder(w).Encode(food)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(food)
 }
 //GET delete food item
 func deleteFood(w http.ResponseWriter, r *http.Request)  {
-
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range foods {
+    if item.id == params["id"] {
+      foods = append(foods[:index], foods[index+1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(food)
 }
 
 func main()  {
