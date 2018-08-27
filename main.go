78e61752fb4ab73db1faa9033136646ed5d3b353
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-26 18:54:30 -0600

    Implements cors middleware

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
+	"math/rand"
+	"github.com/rs/cors"
 )
 
 // Init foods var as a slice food struct
@@ -25,83 +28,94 @@ func getFoods(w http.ResponseWriter, r *http.Request)  {
   w.Header().Set("Content-Type", "application/json")
   json.NewEncoder(w).Encode(foods)
 }
-// //GET single food
-// func getFood(w http.ResponseWriter, r *http.Request)  {
-//   w.Header().Set("Content-Type", "application/json")
-//   params := mux.Vars(r) //get params
-//   //Loop through books and find with id
-//   for _, item := range foods {
-//     if item.ID == params["id"] {
-//       json.NewEncoder(w).Encode(item)
-//       return
-//     }
-//   }
-//   json.NewEncoder(w).Encode(&Food{})
-// }
-// //POST create food item
-// func createFood(w http.ResponseWriter, r *http.Request)  {
-//   w.Header().Set("Content-Type", "application/json")
-//   var food Food
-//   _ = json.NewDecoder(r.Body).Decode(&food)
-//   food.ID = strconv.Itoa(rand.Intn(10000)) //Mock ID - not safe for production
-//   foods = append(foods, food)
-//   json.NewEncoder(w).Encode(food)
-// }
+//GET single food
+func getFood(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r) //get params
+  //Loop through books and find with id
+  for _, item := range foods {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Food{})
+}
+//POST create food item
+func createFood(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+  var food Food
+  _ = json.NewDecoder(r.Body).Decode(&food)
+  food.ID = strconv.Itoa(rand.Intn(10000)) //Mock ID - not safe for production
+  foods = append(foods, food)
+  json.NewEncoder(w).Encode(food)
+}
 
-//PUT update food item
-// func UpdateFood(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	params := mux.Vars(r)
-// 	// fmt.Printf("params: %+v\n", params)
-// 	for index, item := range foods {
-// 		// fmt.Printf("item: %+v\n", item)
-// 		if item.ID == params["id"] {
-// 			var inputFood Food
-// 			err := json.NewDecoder(r.Body).Decode(&inputFood)
-//       fmt.Printf("decode: %+v\n", inputFood)
-// 			if err != nil {
-// 				w.WriteHeader(http.StatusBadRequest)
-// 				w.Write([]byte(err.Error()))
-// 				return
-// 			}
-//       if inputFood.ID != item.ID {
-// 				w.WriteHeader(http.StatusBadRequest)
-// 				w.Write([]byte("ids do not match"))
-// 				return
-// 			}
-// 			foods[index] = inputFood
-// 			json.NewEncoder(w).Encode(inputFood)
-// 			return
-// 		}
-// 	}
-// 	w.WriteHeader(http.StatusNotFound)
-// 	w.Write([]byte("Food not found"))
-// }
+// PUT update food item
+func UpdateFood(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// fmt.Printf("params: %+v\n", params)
+	for index, item := range foods {
+		// fmt.Printf("item: %+v\n", item)
+		if item.ID == params["id"] {
+			var inputFood Food
+			err := json.NewDecoder(r.Body).Decode(&inputFood)
+      fmt.Printf("decode: %+v\n", inputFood)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+      if inputFood.ID != item.ID {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("ids do not match"))
+				return
+			}
+			foods[index] = inputFood
+			json.NewEncoder(w).Encode(inputFood)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Food not found"))
+}
 
-//
-// //GET delete food item
-// func deleteFood(w http.ResponseWriter, r *http.Request)  {
-//   w.Header().Set("Content-Type", "application/json")
-//   params := mux.Vars(r)
-//   for index, item := range foods {
-//     if item.ID == params["id"] {
-//       foods = append(foods[:index], foods[index+1:]...)
-//       break
-//     }
-//   }
-//   json.NewEncoder(w).Encode(food)
-// }
+
+//GET delete food item
+func deleteFood(w http.ResponseWriter, r *http.Request)  {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range foods {
+    if item.ID == params["id"] {
+      foods = append(foods[:index], foods[index+1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(foods)
+}
 
 func main() {
 	//Init Router
 	router := mux.NewRouter()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte("{\"hello\": \"world\"}"))
+	})
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8080", handler)
 
 	//Route handlers / endpoints
-	// router.HandleFunc("/api/v1/foods", getFoods).Methods("GET")
-	// router.HandleFunc("/api/v1/foods/{id}", getFood).Methods("GET")
-	// router.HandleFunc("/api/v1/foods", createFood).Methods("POST")
+	router.HandleFunc("/api/v1/foods", getFoods).Methods("GET")
+	router.HandleFunc("/api/v1/foods/{id}", getFood).Methods("GET")
+	router.HandleFunc("/api/v1/foods", createFood).Methods("POST")
 	router.HandleFunc("/api/v1/foods/{id}", UpdateFood).Methods("PUT")
-	// router.HandleFunc("/api/v1/foods/{id}", deleteFood).Methods("DELETE")
+	router.HandleFunc("/api/v1/foods/{id}", deleteFood).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	// log.Fatal(http.ListenAndServe(":8000", router))
 }
